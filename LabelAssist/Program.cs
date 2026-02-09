59a710ee13a51d0b960dd46bec1c255ae4985839
Evangel Daping <evangeldaping@gmail.com>
2026-02-09 14:04:02 +0000

    Add project files.

---
+++ LabelAssist/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LabelAssist
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            // Enables modern Windows visual styles
+            ApplicationConfiguration.Initialize();
+
+            // Start the main window
+            Application.Run(new MainForm());
+        }
+    }
+}
